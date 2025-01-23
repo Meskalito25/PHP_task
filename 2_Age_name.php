07eae35d8ac32344b55101eee3b9e8426308d522
Meskalito25 <skazlojop.2025@gmail.com>
2025-01-23 23:57:15 +0200

    first commit

---
+++ 2_Age_name.php
@@ -0,0 +1,6 @@
+<?php
+$my_name_is = 'Dima';
+$my_age_is = 40;
+echo $my_name_is . '<br>';
+echo $my_age_is;
+?>
---
+++ 6_Массив с фруктами.php
@@ -0,0 +1,7 @@
+<?php
+$fruits = array("Яблоко", "Банан", "Апельсин");
+
+foreach ($fruits as $fruit) {
+    echo $fruit . "<br>";
+}
+?>
