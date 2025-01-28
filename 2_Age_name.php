22322df24c806a872ed86af3538f3924d95e5b61
Meskalito25 <skazlojop.2025@gmail.com>
2025-01-28 13:02:54 +0200

    <убрал лишние тэги, заменил кавычки в задаче с массивом,убрал лишний код, удалил html из кода,>

--- 2_Age_name.php
+++ 2_Age_name.php
@@ -3,4 +3,3 @@ $my_name_is = 'Dima';
 $my_age_is = 40;
 echo $my_name_is . '<br>';
 echo $my_age_is;
-?>
--- _Массив с фруктами.php
+++ _Массив с фруктами.php
@@ -1,7 +1,7 @@
 <?php
-$fruits = array("Яблоко", "Банан", "Апельсин");
+$fruits =["Яблоко", "Банан", "Апельсин"];
 
 foreach ($fruits as $fruit) {
     echo $fruit . "<br>";
 }
-?>
+
